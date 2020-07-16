3c8aac0f61e2a3e4e47e5fe8cd1be6e4ab84d16b
Bohdan-Nitro <bogdantrogonsky@gmail.com>
2020-07-16 15:21:56 +0300

    Talents section commit()

--- app/js/app.js
+++ app/js/app.js
@@ -52,43 +52,43 @@ $(document).ready(function () {
     });
 
 
-//
-//     //Parallax Scroll
-//     let controller = new ScrollMagic.Controller({
-//         globalSceneOptions: {
-//             triggerHook: 'onLeave',
-//             duration: "300%"
-//         }
-//     });
-//
-// // get all slides
-//     let scenes;
-//     let slides = document.querySelectorAll("section");
-//
-// // create scene for every slide
-//     for (let i = 0; i < slides.length; i++) {
-//
-//         let currentMask = $(slides[i]).find('.scroll__mask');
-//
-//         var maskTween = new TimelineMax();
-//         maskTween.fromTo(currentMask, 1, {opacity:0}, {opacity:2});
-//
-//         scenes = new ScrollMagic.Scene({
-//             triggerElement: slides[i],
-//         }).setTween(maskTween)
-//             // .addIndicators()
-//             .setPin(slides[i], {pushFollowers: false})
-//             .addTo(controller);
-//     }
-//
-//     let slideOffsets = [];
-//
-//     for (let i = 0; i < scenes.length; i++) {
-//         scenes[i].removePin(true);
-//         scenes[i].setPin(slides[i]);
-//         scenes[i].refresh();
-//         slideOffsets.push(Math.ceil(scenes[i].scrollOffset()));
-//     }
+
+    //Parallax Scroll
+    let controller = new ScrollMagic.Controller({
+        globalSceneOptions: {
+            triggerHook: 'onLeave',
+            duration: "300%"
+        }
+    });
+
+// get all slides
+    let scenes;
+    let slides = document.querySelectorAll("section");
+
+// create scene for every slide
+    for (let i = 0; i < slides.length; i++) {
+
+        let currentMask = $(slides[i]).find('.scroll__mask');
+
+        var maskTween = new TimelineMax();
+        maskTween.fromTo(currentMask, 1, {opacity:0}, {opacity:2});
+
+        scenes = new ScrollMagic.Scene({
+            triggerElement: slides[i],
+        }).setTween(maskTween)
+            // .addIndicators()
+            .setPin(slides[i], {pushFollowers: false})
+            .addTo(controller);
+    }
+
+    let slideOffsets = [];
+
+    for (let i = 0; i < scenes.length; i++) {
+        scenes[i].removePin(true);
+        scenes[i].setPin(slides[i]);
+        scenes[i].refresh();
+        slideOffsets.push(Math.ceil(scenes[i].scrollOffset()));
+    }
 
 
     //Macro Section
@@ -128,6 +128,22 @@ $(document).ready(function () {
 
 
     });
+
+    let talentsOptionBtn = '.talents .combo-title';
+    let talentVideoWrapper = '.talents .wrap-content';
+
+    $(document).on('click', talentsOptionBtn, function (e) {
+
+        e.preventDefault();
+        const thisId = $(this).data('id');
+        $(talentVideoWrapper).removeClass('active');
+        $(talentVideoWrapper + '[data-id="' + thisId + '"]').addClass('active');
+
+    });
+
+
+
+
 
 
 });
